319a04347e6a8692b8db15c12897f9290a01d237
Christian Carter <cdcarter@gmail.com>
2011-07-06 22:31:37 -0500

    sms update now deals with improper show IDs

--- updates/sms.js
+++ updates/sms.js
@@ -1,24 +1,28 @@
 function(doc,req) {  
-  var count = Number(req.form.Body.split(" ")[1]);
-  
-  var whoops = false;
-  
-  for(var i = 0, _len = doc.shows.length; i < _len; ++i) {
-    if (doc.shows[i].count === null) {
-      doc.shows[i].count = count;
-      break;
-    }
-    if (i+1 === _len) {
-      whoops = true;
-    }
-  }
-  
   var message;
   
-  if (whoops) {
-    message = "That show doesn't have any more times to enter!";
+  if (doc === null) {
+    message = "No such show, friend!"
   } else {
-    message = "Got it, comrade!";
+    var count = Number(req.form.Body.split(" ")[1]);
+    
+    var whoops = false;
+    
+    for(var i = 0, _len = doc.shows.length; i < _len; ++i) {
+      if (doc.shows[i].count === null) {
+        doc.shows[i].count = count;
+        break;
+      }
+      if (i+1 === _len) {
+        whoops = true;
+      }
+    }
+    
+    if (whoops) {
+      message = "That show doesn't have any more times to enter!";
+    } else {
+      message = "Got it, comrade!";
+    }
   }
   
   var resp =  {
