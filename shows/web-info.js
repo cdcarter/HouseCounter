3be7ce7da15e930f458ae5b85e5172d3e532265a
Christian Carter <cdcarter@gmail.com>
2011-07-07 17:56:38 -0500

    webinfo show func to show all the deets for a show.  has a submit button, pointing to an update func that doesn't exist yet.

---
+++ shows/web-info.js
@@ -0,0 +1,32 @@
+function(doc,req) {
+  var mustache = require("vendor/couchapp/lib/mustache");
+  var path = require("vendor/couchapp/lib/path").init(req);
+  var ddoc = this;
+  
+  doc.avg = 0;
+  doc.perfd = 0;
+  for(var i=0,_len=doc.shows.length; i < _len; ++i) {
+    if (typeof doc.shows[i].count !== "undefined" && doc.shows[i].count !== null){
+      doc.avg = doc.avg + doc.shows[i].count;
+      doc.perfd = doc.perfd + 1
+    }
+    if (doc.shows[i].time[1] == 0) {
+      doc.shows[i].time[1] = "00"
+    }
+    doc.shows[i].time = doc.shows[i].time.join(":")
+    doc.shows[i].idx = i
+  }
+  
+  if (doc.perfd == 0){
+    doc.avg = "None"
+    doc.latest = "None"
+    doc.perfd = "None"
+  } else {
+    doc.avg = doc.avg/doc.perfd
+    doc.latest = doc.shows[doc.perfd-1].count
+  }
+    
+  doc.action = path.update("web",doc._id)
+  
+  return mustache.to_html(ddoc.templates.webinfo,doc)
+}
