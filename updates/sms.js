b7128f79e91754d41e2c43c93be3464587f23e39
Christian Carter <cdcarter@gmail.com>
2011-07-06 22:15:21 -0500

    sms update now updates the next open count slot, and errors if there are none left

--- updates/sms.js
+++ updates/sms.js
@@ -1,12 +1,32 @@
-function(doc,req) {
+function(doc,req) {  
+  var count = Number(req.form.Body.split(" ")[1]);
+  
+  var whoops = false;
+  
+  for(var i = 0, _len = doc.shows.length; i < _len; ++i) {
+    if (doc.shows[i].count === null) {
+      doc.shows[i].count = count;
+      break;
+    }
+    if (i+1 === _len) {
+      whoops = true;
+    }
+  }
+  
+  var message;
+  
+  if (whoops) {
+    message = "That show doesn't have any more times to enter!";
+  } else {
+    message = "Got it, comrade!";
+  }
+  
   var resp =  {
     "headers" : {
       "Content-Type" : "application/xml"
     },
-    "body" : "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n <Response><Sms>Got it, comrade!</Sms></Response>"
+    "body" : "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n <Response><Sms>"+message+"</Sms></Response>"
   };
-  
-  doc.count = Number(req.form.Body.split(" ")[1]);
   
   return [doc,resp];
 }
