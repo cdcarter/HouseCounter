fe796c2b32439b572cbd69ab7a8da7914b7bab6a
Christian Carter <cdcarter@gmail.com>
2011-07-06 21:13:00 -0500

    made the handler an update func

--- updates/handler.js
+++ updates/handler.js
@@ -1,9 +1,9 @@
 function(doc,req) {
   if (req.form.Body.split(" ")[0] === "info"){
     var url = "http://cdcarter.iriscouch.com/sms/_design/sms/_show/info/"+req.form.Body.split(" ")[1]
-    return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Redirect>"+url+"</Redirect></Response>"
+    return [null,"<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Redirect>"+url+"</Redirect></Response>"]
   } else {
     var url = "http://cdcarter.iriscouch.com/sms/_design/sms/_update/sms/" + req.form.Body.split(" ")[0]
-    return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Redirect>"+url+"</Redirect></Response>"
+    return [null,"<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Redirect>"+url+"</Redirect></Response>"]
   }
 }
