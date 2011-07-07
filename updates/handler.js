04bf3b00138b20eb17f0420ec355c37891941e64
Christian Carter <cdcarter@gmail.com>
2011-07-06 21:24:34 -0500

    handler has a matcher now

--- updates/handler.js
+++ updates/handler.js
@@ -1,9 +1,20 @@
 function(doc,req) {
-  if (req.form.Body.split(" ")[0] === "info"){
-    var url = "http://cdcarter.iriscouch.com/sms/_design/sms/_show/info/"+req.form.Body.split(" ")[1]
-    return [null,"<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Redirect>"+url+"</Redirect></Response>"]
+  var url;
+  
+  var handler = {
+    "info": "http://cdcarter.iriscouch.com/sms/_design/sms/_show/info/"+req.form.Body.split(" ")[1],
+    "hot": "http://cdcarter.iriscouch.com/sms/_design/sms/_list/hot/hot",
+    "sellouts": "http://cdcarter.iriscouch.com/sms/_design/sms/_list/sellouts/sellouts",
+    "no-match": "http://cdcarter.iriscouch.com/sms/_design/sms/_update/sms/" + req.form.Body.split(" ")[0]
+  };
+  
+  var verb = req.form.Body.split(" ")[0];
+  
+  if (typeof handler[verb] !== "undefined" && handler[verb] !== null){
+    url = handler[verb];
   } else {
-    var url = "http://cdcarter.iriscouch.com/sms/_design/sms/_update/sms/" + req.form.Body.split(" ")[0]
-    return [null,"<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Redirect>"+url+"</Redirect></Response>"]
+    url = handler["no-match"];
   }
+  
+  return [null,"<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Redirect>"+url+"</Redirect></Response>"]
 }
