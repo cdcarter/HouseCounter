56da51ba4dcfe21b4388e3b578529a7408a96d81
Christian Carter <cdcarter@gmail.com>
2011-07-06 22:27:12 -0500

    awesome rockin show function to get the latest and avg ticket sales for a show.  404s gracefully.

--- shows/info.js
+++ shows/info.js
@@ -1,4 +1,27 @@
 function(doc,req) {
-  var data = "Show: " + doc.name + " - " + doc.count;
-  return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n <Response><Sms>"+data+"</Sms></Response>"
+  if (doc == null) {
+    var data = "Show not found, dude!"
+    return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n <Response><Sms>"+data+"</Sms></Response>"
+  } else {
+    var mustache = require("vendor/couchapp/lib/mustache");
+    
+    var template = "Show: {{name}} @ {{venue}}-Avg:{{avg}},Latest:{{latest}}"
+    
+    doc.avg = 0;
+    doc.perfd = 0;
+    for(var i=0,_len=doc.shows.length; i < _len; ++i) {
+      if (typeof doc.shows[i].count !== "undefined" && doc.shows[i].count !== null){
+        doc.avg = doc.avg + doc.shows[i].count;
+        doc.perfd = doc.perfd + 1
+      }
+    }
+    
+    doc.avg = doc.avg/doc.perfd
+    
+    doc.latest = doc.shows[doc.shows.length-1].count
+    
+    var data = mustache.to_html(template,doc)
+    
+    return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n <Response><Sms>"+data+"</Sms></Response>"
+  }
 }
