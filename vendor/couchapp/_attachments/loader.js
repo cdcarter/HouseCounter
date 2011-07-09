0aae4a3fd9d29b1988dff73dac897bf2c3b62321
Christian Carter <cdcarter@gmail.com>
2011-07-09 15:21:39 -0500

    cleaning up vendor, and getting ready to have processing integration

--- vendor/couchapp/_attachments/loader.js
+++ vendor/couchapp/_attachments/loader.js
@@ -13,5 +13,4 @@ couchapp_load([
   "vendor/couchapp/jquery.couch.app.js",
   "vendor/couchapp/jquery.couch.app.util.js",
   "vendor/couchapp/jquery.mustache.js",
-  "vendor/couchapp/jquery.evently.js"
 ]);
