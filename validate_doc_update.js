51a9be5a64dd6a0ef846a8ac147799ddfa30f7dc
Christian Carter <cdcarter@gmail.com>
2011-07-08 20:40:26 -0500

    now out of order shows cannot be created

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -1,3 +1,10 @@
-function(newDoc,savedDoc,userCtx) {
-
+function (newDoc,savedDoc,userCtx) {
+  var _ = require("vendor/underscore");
+  if (newDoc.type=="show") {
+    var unsorted = _.clone(newDoc.shows)
+    var sorted = newDoc.shows.sort(function(x,y){return x.slot-y.slot})
+    if (!_.isEqual(sorted,unsorted)) {
+      throw {forbidden: "Not sorted"};
+    }
+  }
 }
