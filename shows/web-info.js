21dcb000a8c6a4f879a7dc9abdfe602f803d5549
Christian <cdcarter@gmail.com>
2011-07-27 01:04:28 -0500

    volunteer prediction all hooked up to the web info show

--- shows/web-info.js
+++ shows/web-info.js
@@ -1,28 +1,12 @@
 function(doc,req) {
-  var mustache = require("vendor/couchapp/lib/mustache");
-  var path = require("vendor/couchapp/lib/path").init(req);
   var ddoc = this;
   
-  doc.avg = 0;
-  doc.perfd = 0;
-  for(var i=0,_len=doc.shows.length; i < _len; ++i) {
-    if (typeof doc.shows[i].count !== "undefined" && doc.shows[i].count !== null){
-      doc.avg = doc.avg + doc.shows[i].count;
-      doc.perfd = doc.perfd + 1
-    }
-    doc.shows[i].idx = i
-    
-    doc.shows[i].time = ddoc.data.slots[doc.shows[i].slot]
-  }
+  var mustache = require("vendor/couchapp/lib/mustache");
+  var path = require("vendor/couchapp/lib/path").init(req);
+  var calc = require("vendor/showmath").init(ddoc);
   
-  if (doc.perfd == 0){
-    doc.avg = "None"
-    doc.latest = "None"
-    doc.perfd = "None"
-  } else {
-    doc.avg = doc.avg/doc.perfd
-    doc.latest = doc.shows[doc.perfd-1].count
-  }
+  doc = calc.avg(doc);
+  doc.guess = calc.guess(doc);
     
   doc.action = path.rewrite(doc._id)
   doc.list = path.rewrite()
