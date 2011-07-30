47fa43fba6c01bcd3891dfe975bfcbc2d6f54acf
Christian <cdcarter@gmail.com>
2011-07-30 17:02:23 -0500

    the shows view now uses the showmath module!

--- views/shows/map.js
+++ views/shows/map.js
@@ -1,23 +1,14 @@
 function(doc) {
   if (doc.type == "show") {
-    avg = 0;
-    perfd = 0;
-    for(var i=0,_len=doc.shows.length; i < _len; ++i) {
-      if (typeof doc.shows[i].count !== "undefined" && doc.shows[i].count !== null){
-        avg = avg + doc.shows[i].count;
-        perfd = perfd + 1
-      }
+    var calc = require("views/lib/showmath").init()
+    avgs = calc.avg(doc)
+    
+    if (avgs.perfd==0) {
+      avgs.perfd = "None";
+      avgs.avg = "None";
+      avgs.latest = "None";
     }
     
-    if (perfd == 0){
-      avg = "None"
-      latest = "None"
-      perfd = "None"
-    } else {
-      avg = avg/perfd
-      latest = doc.shows[perfd-1].count
-    }
-    
-    emit(doc.venue,{"name":doc.name, "avg": avg, "latest": latest, "perfd": perfd, "id": doc._id})
+    emit(doc.venue,{"name":doc.name, "avg": avgs.avg, "latest": avgs.latest, "perfd": avgs.perfd, "id": doc._id})
   }
 }
