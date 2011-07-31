f99e0ce500b606eb57b235c97bfd1e44dbc00ecd
Christian <cdcarter@gmail.com>
2011-07-30 19:20:51 -0500

    some dumb fixes

--- views/shows/map.js
+++ views/shows/map.js
@@ -1,8 +1,8 @@
 function(doc) {
   if (doc.type == "show") {
     var calc = require("views/lib/showmath").init()
-    avgs = calc.avg(doc)
-    guess = calc.guess(doc,avgs)
+    var avgs = calc.avg(doc);
+    var guess = calc.guess(doc,avgs);
     
     emit(doc.venue,{"name":doc.name, "avgs": avgs, "id": doc._id,"guess":guess})
   }
